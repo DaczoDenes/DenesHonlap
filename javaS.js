60efd196aa18538771098735dd8b1738332bcccb
DaczoDenes <63414642+DaczoDenes@users.noreply.github.com>
2024-09-20 13:25:03 +0200

    Színváltó háttér

--- javaS.js
+++ javaS.js
@@ -1,4 +1,4 @@
-
+/*
 $.ajax({
     url: 'https://randomuser.me/api/',
     dataType: 'json',
@@ -6,3 +6,24 @@ $.ajax({
       console.log(data);
     }
   });
+  */
+
+let x = -1;
+
+function switchBackground(){
+  x++;
+  if (x>2){
+    x = 0;
+  }
+  switch(x){
+    case 0:
+      document.body.style.backgroundColor = "#14732B";
+      break;
+    case 1:
+      document.body.style.backgroundColor = "#01260A";
+      break;
+    case 2:
+      document.body.style.backgroundColor = "black";
+      break;
+  }
+}
