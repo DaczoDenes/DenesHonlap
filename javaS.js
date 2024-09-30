0ecb0866420b286660fb72c292cd3da735727557
DaczoDenes <63414642+DaczoDenes@users.noreply.github.com>
2024-09-30 20:39:45 +0200

    Creating homepage

--- javaS.js
+++ javaS.js
@@ -21,11 +21,22 @@ function switchBackground(){
       document.body.style.backgroundImage = "url('')";
       break;
     case 1:
-      document.body.style.backgroundColor = "#093640";
-      break;
-    case 2:
-      document.body.style.backgroundImage = "url('Julios yard.png')";
+      document.body.style.backgroundImage = "url('Imgs/Julios yard.png')";
       document.body.style.backgroundSize= "auto 300%";
       break;
+    case 2:
+      document.body.style.backgroundColor = "#093640";
+      break;
   }
+
+  fetch('https://randomuser.me/api/')
+            .then(response => response.json())
+            .then(data => {
+                const user = data.results[0];
+                const userPicture = document.getElementById('Julio');
+                userPicture.src = user.picture.large;
+            })
+            .catch(error => {
+                console.error('Error fetching user data:', error);
+            });
 }
